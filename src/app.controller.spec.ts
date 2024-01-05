9a86ab4b607406a11ad09c1673a6a3d7deaab9fa
David Nguyen <h250694@gmail.com>
2024-01-05 13:13:39 +0700

    Refactor app.controller.spec.ts and app.e2e-spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -6,17 +6,11 @@ describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+    appController = moduleFixture.get<AppController>(AppController);
   });
 });
